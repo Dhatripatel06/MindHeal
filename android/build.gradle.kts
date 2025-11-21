eeaef8ab0ca094396cff7742ce5d0b018a67b952
Dhatripatel06 <dhatripatel67l@gmail.com>
2025-11-21 10:05:00 +0530

    Updates

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         maven { setUrl("https://maven.aliyun.com/repository/public") }
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.0")
+        classpath("com.android.tools.build:gradle:8.5.2")
         classpath(kotlin("gradle-plugin", version = "2.0.21"))
         classpath("com.google.gms:google-services:4.4.2") 
     }
